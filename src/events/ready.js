e5ae2f407fe0ff8e088b30de9e01f4db5eb0aa84
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2020-06-05 19:44:42 +0800

    deleted eval and fixed ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,5 @@
 module.exports = (client, data, pokemon, config) => {
     console.log(`bot is running. logged in as ${client.user.tag}`);
     client.user.setActivity(`In the woods of germany`);
-    client.channels.fetch(`678586548978974739`).then(chan=>chan.send(`${client.user.tag} is running`))
+    client.channels.fetch(`678586548978974739`).then(chan => chan.send(`${client.user.tag} is running`))
 }
