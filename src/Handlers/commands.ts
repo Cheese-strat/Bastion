4899bccc35c950c69d91a95b2d903dae4f665f80
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2020-06-24 15:49:41 +0800

    Update commands.ts

--- src/Handlers/commands.ts
+++ src/Handlers/commands.ts
@@ -1,12 +1,12 @@
 'use strict';
-import {readdirSync} from "fs";
-export function (client:any) {
+import { readdirSync } from "fs";
+export const fn = (client: any) => {
     for (const Folder of readdirSync(`${client.path}/commands`).filter(folder => !folder.includes("."))) {
         for (const fileName of readdirSync(`${client.path}/commands/${Folder}`).filter(file => file.endsWith(".ts"))) {
             const commandOBJ = require(`${client.path}/commands/${Folder}/${fileName}`)
             commandOBJ.category = Folder
             client.commands.set(commandOBJ.name, commandOBJ)
             console.log(`found command: ${commandOBJ.name}`)
-        }
-      }
-}
+        };
+    };
+};
