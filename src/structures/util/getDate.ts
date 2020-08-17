1c874101672205ddb3805bed4568f4ea6e491f07
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2020-08-17 12:42:13 +0800

    adding some regex

--- src/structures/util/getDate.ts
+++ src/structures/util/getDate.ts
@@ -1,3 +1,9 @@
-export default (time:string):number => {
-    return Number(time)
+export default (str:string):number => {
+    str.toLowerCase()
+    for (let x =0;x<Object.keys(obj).length;x++){
+        str.replace(Object.keys(obj)[x], obj[Object.keys(obj)[x]]) 
+    }
+    const matches = str.match(/[0-9]+ {0,}[smhd]/gi)
+    console.log(matches)
+    return matches
 }
