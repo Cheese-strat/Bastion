20b491685b55cb71c3b878ae169884ca8cb60d8d
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2020-08-17 17:38:58 +0800

    type guards ig

--- src/structures/util/storage.ts
+++ src/structures/util/storage.ts
@@ -5,15 +5,15 @@ type writing = (path:string, guildID:string, data:storageGuildTYPE)=>storageGuil
 type singleGuild = (path:string, guildID:string)=>storageGuildTYPE
 type allGuilds = (path:string, guildID:null)=>storageTYPE
 
-export const storage:writing|singleGuild|allGuilds = (path, guildID, data) => {
+export const storage = (path:string, guildID: string | null, data?: object):storageGuildTYPE|storageTYPE => {
     const json: storageTYPE = require(`${path}storage.json`);
-    if (!guildID) return json
-    if (!data) {
+    if (typeof guildID !== "string") return json
+    if (typeof data === "undefined") {
         if (!Object.keys(json).includes(guildID)){
             return storage(path, guildID, defaultOBJ)
         }
         return json[guildID] as storageGuildTYPE
-    }
+    } else this
     Object.defineProperty(json, guildID, data as object);
     let queue = Promise.resolve();
 
