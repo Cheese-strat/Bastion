dc6d61babdbe58de3ba509bfc06d525a3cb2663a
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2020-08-28 17:09:56 +0800

    overload functions!

--- src/structures/util/storage.ts
+++ src/structures/util/storage.ts
@@ -1,19 +1,18 @@
 import { writeFile } from 'fs';
-import { storageTYPE, storageGuildTYPE } from '../library';
+import { storageTYPE, storageGuildTYPE , guildObject} from '../library';
 
-type writing = (path:string, guildID:string, data:storageGuildTYPE)=>storageGuildTYPE
-type singleGuild = (path:string, guildID:string)=>storageGuildTYPE
-type allGuilds = (path:string, guildID:null)=>storageTYPE
-
-export const storage = (path:string, guildID: string | null, data?: object):storageGuildTYPE|storageTYPE => {
+export function storage(path:string, guildID?:never, data?:never):storageTYPE
+export function storage(path:string, guildID:string, data?:never):storageGuildTYPE
+export function storage(path:string, guildID:string, data:storageGuildTYPE):storageGuildTYPE
+export function storage(path:string, guildID:any, data?: any):any {
     const json: storageTYPE = require(`${path}storage.json`);
     if (typeof guildID !== "string") return json
     if (typeof data === "undefined") {
         if (!Object.keys(json).includes(guildID)){
-            return storage(path, guildID, defaultOBJ)
+            return storage(path, guildID, guildObject )
         }
         return json[guildID] as storageGuildTYPE
-    } else this
+    }
     Object.defineProperty(json, guildID, data as object);
     let queue = Promise.resolve();
 
