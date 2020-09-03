f24e5fbfd9cdb49e2ce1b5d8a9fb31876e492f72
Cheese-strat <46002480+Cheese-strat@users.noreply.github.com>
2020-09-03 21:35:13 +0800

    typo ;)

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,6 @@
 import { clientClass } from "../structures/library";
 export default async (client: clientClass) => {
-    if (client.user === null) return console.log(`the clientUser iis not logged in yet`);
+    if (client.user === null) return console.log(`the clientUser is not logged in yet`);
     console.log(`bot is running. logged in as ${client.user.tag}`);
     await client.user.setPresence({activity: {name: 'on the floor'}, status: 'idle'});
     const channel = await client.getLogChannel()
